872713e9f41e53aa71d21fa923aff834f2758be5
Hesham <Hesham Abdelnasser@DESKTOP-D15PJJA>
2025-03-18 03:02:10 +0200

    v3 with out seo + edit image + social

--- Alhoda/src/components/Fotter/Fotter.jsx
+++ Alhoda/src/components/Fotter/Fotter.jsx
@@ -1,12 +1,12 @@
 import "./Fotter.css";
-import { BsSnapchat } from "react-icons/bs";
 import { FaTiktok } from "react-icons/fa6";
 import { FaInstagram } from "react-icons/fa";
-import { FaXTwitter } from "react-icons/fa6";
 import { IoIosArrowBack } from "react-icons/io";
 import { FaPhoneAlt } from "react-icons/fa";
 import { IoMail } from "react-icons/io5";
 import { FaMapMarkerAlt } from "react-icons/fa";
+import { FaWhatsapp  } from 'react-icons/fa';
+import { FaFacebookF } from "react-icons/fa";
 
 
 
@@ -14,6 +14,7 @@ export default function Footer() {
   return (
     <>
       <footer className="footer">
+        
         <div className="footerContent">
           <div className="footerSection">
             <div>
@@ -70,18 +71,46 @@ export default function Footer() {
             <p className="aboutText">الخيارات للاثاث و غرف النوم و الكنب و المطابخ  بأعلى جودة و افضل سعر في الرياض </p>
             <br/>
             <div className="socialIcons">
-              <div className="iconStyle1">
-                    <BsSnapchat />
-                  </div>
-                  <div className="iconStyle1">
-                    <FaTiktok />
-                  </div>
-                  <div className="iconStyle1">
-                    <FaInstagram />
-                  </div>
-                  <div className="iconStyle1">
-                    <FaXTwitter />
-                  </div>
+            <div className="iconStyle">
+          <a
+              href="https://wa.me/+966598608273"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ color: "white", display: "flex", alignItems: "center" }}
+            >
+              <FaWhatsapp size={20} />
+            </a>            
+          </div>
+          <div className="iconStyle">
+            <a
+              href=" http://tiktok.com/@saody003"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ color: "white", display: "flex", alignItems: "center" }}
+            >
+              <FaTiktok size={20} />
+            </a>
+          </div>
+          <div className="iconStyle">
+            <a
+              href="https://www.instagram.com/kasb_almawaard?igsh=enRzcmY5cGw4OWo5"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ color: "white", display: "flex", alignItems: "center" }}
+            >
+              <FaInstagram size={20} />
+            </a>
+          </div>
+          <div className="iconStyle">
+            <a
+              href="https://www.facebook.com/people/%D9%85%D8%A4%D8%B3%D8%B3%D8%A9-%D9%86%D9%88%D8%B1-%D8%A7%D9%84%D9%87%D8%AF%D9%8A-%D9%84%D9%84%D8%A3%D8%AB%D8%A7%D8%AB-%D8%A7%D9%84%D8%AC%D8%AF%D9%8A%D8%AF/61555551041774/?rdid=WtbgNiqHtSjlCtiJ&share_url=https%3A%2F%2Fwww.facebook.com%2Fshare%2F158L26ofJD%2F"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ color: "white", display: "flex", alignItems: "center" }}
+            >
+              <FaFacebookF size={20} />
+            </a>
+          </div>
                   
               </div>
           </div>
--- Alhoda/src/components/Header/Header.jsx
+++ Alhoda/src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
 // import { IoIosArrowDown } from "react-icons/io";
 // import { TbWorld } from "react-icons/tb";
 import { BsSnapchat } from "react-icons/bs";
-import { FaTiktok, FaXTwitter } from "react-icons/fa6";
+import { FaTiktok } from "react-icons/fa6";
 import { FaInstagram } from "react-icons/fa";
 import { FaPhoneAlt } from "react-icons/fa";
+import { FaFacebookF } from "react-icons/fa";
+import { FaWhatsapp  } from 'react-icons/fa';
 
 import "./Header.css";
 import FloatingButtons from "../buttons/floatingButtons";
@@ -31,23 +33,54 @@ export default function Header() {
         <div></div>
         <div className="icons">
           <div className="iconStyle">
-            <BsSnapchat />
+          <a
+              href="https://wa.me/+966598608273"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ color: "black", display: "flex", alignItems: "center" }}
+            >
+              <FaWhatsapp size={18} />
+            </a>            
           </div>
           <div className="iconStyle">
-            <FaTiktok />
+            <a
+              href=" http://tiktok.com/@saody003"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ color: "black", display: "flex", alignItems: "center" }}
+            >
+              <FaTiktok size={18} />
+            </a>
           </div>
           <div className="iconStyle">
-            <FaInstagram />
+            <a
+              href="https://www.instagram.com/kasb_almawaard?igsh=enRzcmY5cGw4OWo5"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ color: "black", display: "flex", alignItems: "center" }}
+            >
+              <FaInstagram size={20} />
+            </a>
           </div>
           <div className="iconStyle">
-            <FaXTwitter />
+            <a
+              href="https://www.facebook.com/people/%D9%85%D8%A4%D8%B3%D8%B3%D8%A9-%D9%86%D9%88%D8%B1-%D8%A7%D9%84%D9%87%D8%AF%D9%8A-%D9%84%D9%84%D8%A3%D8%AB%D8%A7%D8%AB-%D8%A7%D9%84%D8%AC%D8%AF%D9%8A%D8%AF/61555551041774/?rdid=WtbgNiqHtSjlCtiJ&share_url=https%3A%2F%2Fwww.facebook.com%2Fshare%2F158L26ofJD%2F"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ color: "black", display: "flex", alignItems: "center" }}
+            >
+              <FaFacebookF size={20} />
+            </a>
           </div>
         </div>
       </div>
-      <button onClick={handleCall} className="btn2  btn-primary d-flex align-items-center justify-content-center gap-3" >
-                اتصل بنا
-                <FaPhoneAlt />
-                  </button>
+      <button
+        onClick={handleCall}
+        className="btn2  btn-primary d-flex align-items-center justify-content-center gap-3"
+      >
+        اتصل بنا
+        <FaPhoneAlt />
+      </button>
       <FloatingButtons />
     </div>
   );
